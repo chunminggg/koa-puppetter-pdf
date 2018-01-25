21ad0a740d3d9e64ad3e6f7ea3f77eac6641518b
neo-mac <miaobingliang@neotrident.com>
2018-01-25 09:09:54 +0800

    done

--- middleare/pdfGenerate.js
+++ middleare/pdfGenerate.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 var AV = require('leancloud-storage');
 var fs = require('fs')
+const previewUrl = 'http://eln.ilabpower.com:8020/cloud-community/detailExp/html/mobileExpPrint.html?'
 exports.save =  async (ctx,next) => {
     let req = ctx.request.body
     const browser = await puppeteer.launch();
@@ -12,7 +13,7 @@ exports.save =  async (ctx,next) => {
         domain: '.labpower.com'
     })
     await page.setJavaScriptEnabled(true)
-    await page.goto(`http://eln.ilabpower.com:8020/cloud-community/detailExp/html/mobileExpPrint.html?${req.guid}&${req.userId}`);
+    await page.goto(`${previewUrl}${req.guid}&${req.userId}`);
     await page.waitForSelector('.nodeCompleted')
     var pdfData = await page.pdf({path: `${req.guid}.pdf`, format: 'A4'});
     var fileResult = await new AV.File(`${req.guid}.pdf`,pdfData).save()
