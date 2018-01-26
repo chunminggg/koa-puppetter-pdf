850ac2dc98d347d219a2f232e8c9c065f332347a
chunminggg <651403538@qq.com>
2018-01-26 22:10:59 +0800

    done

--- middleare/pdfGenerate.js
+++ middleare/pdfGenerate.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 var AV = require('leancloud-storage');
-var fs = require('fs')
+const previewUrl = ''//网址
 exports.save =  async (ctx,next) => {
     let req = ctx.request.body
     const browser = await puppeteer.launch();
@@ -9,10 +9,13 @@ exports.save =  async (ctx,next) => {
         name: 'ucStorage',
         value: req.ucStorage,
         path: '/',
-        domain: '.labpower.com'
+        domain: '.xxx.com',//配域名
+        httpOnly:false,
+        secure:true,
     })
+
     await page.setJavaScriptEnabled(true)
-    await page.goto(`http://eln.ilabpower.com:8020/cloud-community/detailExp/html/mobileExpPrint.html?${req.guid}&${req.userId}`);
+    await page.goto(`${previewUrl}${req.guid}`);
     await page.waitForSelector('.nodeCompleted')
     var pdfData = await page.pdf({path: `${req.guid}.pdf`, format: 'A4'});
     var fileResult = await new AV.File(`${req.guid}.pdf`,pdfData).save()
