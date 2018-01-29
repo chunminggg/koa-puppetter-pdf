c7aadc680dbccb975333256b8db1c7da093e6d9e
neo-mac <miaobingliang@neotrident.com>
2018-01-29 09:31:23 +0800

    no message

--- middleare/pdfGenerate.js
+++ middleare/pdfGenerate.js
@@ -16,6 +16,7 @@ exports.save =  async (ctx,next) => {
     await page.goto(`${previewUrl}${req.guid}&${req.userId}`);
     await page.waitForSelector('.nodeCompleted')
     var pdfData = await page.pdf({path: `${req.guid}.pdf`, format: 'A4'});
+
     var fileResult = await new AV.File(`${req.guid}.pdf`,pdfData).save()
     await browser.close();
     return ctx.body = fileResult.attributes.url
