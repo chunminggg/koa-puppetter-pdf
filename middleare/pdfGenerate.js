d09329bdb6df6a1dd74f3b49362e893092c5824f
neo-mac <miaobingliang@neotrident.com>
2018-02-13 16:22:37 +0800

    done

--- middleare/pdfGenerate.js
+++ middleare/pdfGenerate.js
@@ -1,23 +1,24 @@
 const puppeteer = require('puppeteer');
 var AV = require('leancloud-storage');
-const previewUrl = ''//网址
+const previewUrl = 'http://eln.ilabpower.com:8020/cloud-community/detailExp/html/mobileExpPrint.html?'//网址
 exports.save =  async (ctx,next) => {
     let req = ctx.request.body
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
     page.setCookie({
-        name: 'token',
-        value: req.token,
+        name: 'ucStorage',
+        value: req.ucStorage,
         path: '/',
-        domain: '.xxx.com',//配域名
+        domain: '.ilabpower.com',//配域名
         httpOnly:false,
         secure:true,
     })
     await page.setJavaScriptEnabled(true)
-    await page.goto(`${传入网址}`);
+    await page.goto(`${previewUrl}${req.guid}&${req.userId}`);
     await page.waitForSelector('.nodeCompleted')
     var pdfData = await page.pdf({path: `${req.guid}.pdf`, format: 'A4'});
-
+    
     var fileResult = await new AV.File(`${req.guid}.pdf`,pdfData).save()
     await browser.close();
     return ctx.body = fileResult.attributes.url
